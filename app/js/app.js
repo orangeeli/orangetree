ec897b8ae58862ddf7a41deab461d6216694be02
Eliseu Martinho <eliseu.martinho@gmail.com>
2016-06-22 07:26:22 +0000

    NodeJS project

---
+++ app/js/app.js
@@ -0,0 +1,27 @@
+(function(wd, doc, m){
+
+  var skills = ["Seasoned Software Engineer", "Motorcycle Lover", "Crossfit Enthusiast", "Folk Dancing amateur"];
+
+  function onLoad(){
+
+    var profession = doc.querySelector(".profession");
+
+    function flip(){
+      profession.innerText = skills[random(skills.length-1)];
+    }
+
+    function random(max) {
+      return m.floor(m.random() * max);
+    }
+    wd.setInterval(flip, 5000);
+
+    var year = doc.querySelector(".year");
+    year.innerText = new Date().getFullYear()+"";
+
+  }
+
+  console.log("[DEBUG] Selected the document: " + doc);
+
+  doc? doc.addEventListener("DOMContentLoaded", onLoad, false) : console.log("[DEBUG] No document.");
+
+})(window, document, Math);
---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+(()=>{
+
+  // TODO : each task should go to each separate module
+
+  'use strict';
+
+  // Upgrade to node 6 or use babel for import usage
+  const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    pug = require('gulp-pug'),
+    mocha = require('gulp-mocha'),
+    pugLinter = require('gulp-pug-linter'),
+    debug = require('gulp-debug'),
+    del = require('del'),
+    nsp = require('gulp-nsp'),
+    browserify = require('browserify'),
+    uglify = require('gulp-uglify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer');
+
+  const tasks = {
+
+    nsp(cb){
+      nsp({package: __dirname + '/package.json'}, cb);
+    },
+
+    browserify(){
+      return browserify("./app/js/app.js")
+        .bundle()
+        .pipe(source("app.js"))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest("./public/js"));
+    },
+
+    sass(){
+      return gulp.src('./app/css/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
+    },
+
+    pug () {
+      return gulp.src('./app/**/*.pug')
+        .pipe(debug({title: 'unicorn:'}))
+        .pipe(pug({
+          pretty: true
+        }))
+        .pipe(gulp.dest('public'));
+    },
+    mocha (){
+      return gulp.src(['./test/**/*.js'], {read: false})
+        // gulp-mocha needs filepaths so you can't have any plugins before it
+        .pipe(mocha({
+          reporter: 'nyan',
+          recursive: true
+        })).once('error', function () {
+          process.exit(1);
+        });
+    },
+    gulpLint(){
+      return gulp
+        .src('./app/**/*.pug')
+        .pipe(pugLinter())
+        .pipe(pugLinter.reporter());
+    },
+    www : {
+      clean () {
+        return del([
+          'public/**/*'
+        ]);
+      }
+    },
+    partials : {
+      clean () {
+        return del([
+          'public/partials'
+        ]);
+      }
+    }
+
+  };
+
+  // TODO: have another task for deploy
+  gulp.task('compile:sass', tasks.sass);
+  gulp.task('compile:js', tasks.browserify);
+  gulp.task('lint:pug', tasks.pugLinter);
+  gulp.task('compile:pug', tasks.pug);
+  gulp.task('test:mocha', tasks.mocha);
+  gulp.task('clean:www', tasks.www.clean);
+  gulp.task('security:nsp', tasks.nsp);
+
+  gulp.task('default', ['security:nsp', 'clean:www', 'compile:sass', 'lint:pug', 'compile:pug', 'compile:js']);
+
+})();
+
